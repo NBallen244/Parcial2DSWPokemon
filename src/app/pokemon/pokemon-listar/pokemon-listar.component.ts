ea408fd5df0eb94bed49afabee1cfe91b47260e3
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2024-11-20 08:35:46 -0500

    listar pokemones

---
+++ src/app/pokemon/pokemon-listar/pokemon-listar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
+import { PokemonDetailDto } from '../pokemonDetailDto';
+
+@Component({
+  selector: 'app-pokemon-listar',
+  templateUrl: './pokemon-listar.component.html',
+  styleUrls: ['./pokemon-listar.component.css']
+})
+export class PokemonListarComponent implements OnInit {
+
+  pokemones:PokemonDetailDto[]=[];
+  servicio:PokemonService;
+  selected:boolean=false;
+  selectedPokemon!:PokemonDetailDto;
+
+  constructor(servicio:PokemonService) {
+    this.servicio = servicio;
+   }
+
+  getPokemons(){
+    this.pokemones = this.servicio.getPokemons();
+  }
+
+  ngOnInit() {
+    this.getPokemons();
+  }
+
+}
